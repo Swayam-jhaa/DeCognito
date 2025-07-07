23715043dd8e7cda222bf34ef4dffeb08341456c
Swayam-jhaa <swayamjhaoffical@gmail.com>
2025-07-07 21:24:59 +0530

    second

--- app/auth.py
+++ app/auth.py
@@ -1,1 +1,6 @@
+from fastapi import APIRouter, HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt
+from passlib.context import CryptContext
+# … user store, token creation …
 
